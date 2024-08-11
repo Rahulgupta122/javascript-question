a85866b4fff1339430fd984c8332730a12097f31
Rahul Gupta <122372097+Rahulgupta122@users.noreply.github.com>
2024-08-11 17:14:16 +0530

    const variable use

---
+++ const.js
@@ -0,0 +1,12 @@
+// by using this we get this error 
+//Uncaught TypeError: Assignment to constant variable.
+// at const.js:2:2
+// const a=40;
+// a=45
+// console.log(a);
+
+
+
+const a=40;
+
+console.log(a);
